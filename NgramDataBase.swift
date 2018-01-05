9e906c1b0ba2e07ccc9eccc16920b0bf5dcb0aa2
primetimer <primetimertime@gmail.com>
2018-01-05 08:44:58 +0100

    Database

---
+++ NgramDataBase.swift
@@ -0,0 +1,100 @@
+//
+//  NgramDataBase.swift
+//  WordFinder
+//
+//  Created by Stephan Jancar on 05.01.18.
+//
+
+import Foundation
+
+public enum NgramCorpus : Int {
+	case english = 15
+	case german = 20
+
+}
+
+public struct NgramBase {
+	public var year: Int = 0
+	public var words : Int = 0
+	public var pages : Int = 0
+	public var volumes : Int = 0
+}
+
+class NgramDict {
+	
+	var corpus : NgramCorpus!
+	var dict : [Int:NgramBase]!
+
+	init(corpus : NgramCorpus) {
+		self.corpus = corpus
+		dict = [:]
+	}
+	func append(data : NgramBase) {
+		if data.year > 0 {
+			dict[data.year] = data
+		}
+	}
+	func getData(year : Int) -> NgramBase? {
+		return dict[year]
+	}
+}
+
+//Retrieves absolute count Values by google in all corpi
+public class NgramDataBase  {
+	public let allcorpus : [NgramCorpus] = [.english, .german]
+
+	//Singleton
+	static public let shared = NgramDataBase()
+	private (set) var loaded = false
+	private (set) var error : Error? = nil
+	
+	private var corpusdict : [NgramCorpus:NgramDict] = [:]
+	private (set) var urls : [NgramCorpus:String] = [:]
+	public init() {
+		urls[.english] = "https://storage.googleapis.com/books/ngrams/books/googlebooks-eng-all-totalcounts-20120701.txt"
+		urls[.german] = "https://storage.googleapis.com/books/ngrams/books/googlebooks-ger-all-totalcounts-20120701.txt"
+		load()
+	}
+	
+	public func load(reload : Bool = false) {
+		if reload { loaded = false }
+		if loaded { return }
+		
+		corpusdict = [:]
+		error = nil
+		for corpus in allcorpus {
+			if let dict = loadCount(corpus: corpus) {
+				corpusdict[corpus] = dict
+			}
+		}
+		if corpusdict.count == urls.count && error == nil {
+			loaded = true
+		}
+	}
+	
+	private func loadCount(corpus : NgramCorpus) -> NgramDict? {
+		let ans = NgramDict(corpus: corpus)
+		guard let urlstr = urls[corpus] else { return nil}
+		guard let url = URL(string : urlstr) else { return nil }
+		do {
+			let countstr = try String(contentsOf: url)
+			let rows =  countstr.components(separatedBy: "\t")
+			for row in rows {
+				let vals = row.components(separatedBy: ",")
+				if vals.count != 4 { continue }
+				if let year = Int(vals[0]), let words = Int(vals[1]), let pages = Int(vals[2]), let vol = Int(vals[3]) {
+					let entry = NgramBase(year: year, words: words, pages: pages, volumes: vol)
+					ans.append(data: entry)
+					print(corpus,entry)
+				}
+			}
+		}
+		catch let error {
+			// Error handling
+			self.error = error
+			print(error)
+		}
+		return ans
+	}
+}
+
--- NgramDataLoader.swift
+++ NgramDataLoader.swift
@@ -9,21 +9,44 @@ import Foundation
 import UIKit
 
 
+extension String {
+	var lines: [String] {
+		var result: [String] = []
+		enumerateLines { line, _ in result.append(line) }
+		return result
+	}
+}
+
 public class NGramLoader {
 	
-	private (set) var  Data : [[Double]] = []
-	
-	public init() {}
-	/*
-	private var iframe : String {
-	get {
-	return "<iframe name=\"ngram_chart\" src=\"https://books.google.com/ngrams/interactive_chart?content=113100%2C113110&year_start=1960&year_end=2017&corpus=15&smoothing=3\" width=400 height=400 marginwidth=0 marginheight=0 hspace=0 vspace=0 frameborder=0 scrolling=yes></iframe>"
-	}
-	}
-	*/
+	private (set) var  DataRelative : [[Double]] = []
 	
 	private let url = "https://books.google.com/ngrams/interactive_chart?content=113100%2C113110&year_start=1960&year_end=2017&corpus=15&smoothing=3"
 	
+	private var base : NgramDataBase!
+	public init() {
+		base = NgramDataBase.shared
+	}
+	private func parsecount(startIndex : String.Index) -> String.Index? {
+		guard let start = content.range(of: "timeseries\": [", options: .literal, range: startIndex..<content.endIndex, locale: nil)
+			else { return nil }
+		
+		//let trail = content[start.upperBound..<content.endIndex]
+		guard let end = content.range(of: "]", options: .literal, range: start.upperBound..<content.endIndex,locale:nil)
+			else { return nil }
+		
+		let found = content[start.upperBound..<end.lowerBound]
+		//print(found)
+		
+		let data = found.components(separatedBy: ", ")
+		var dvaldata : [Double] = []
+		for d in data {
+			guard let dval = Double(d) else { break }
+			dvaldata.append(dval)
+		}
+		DataRelative.append(dvaldata)
+		return end.upperBound
+	}
 	private func parsecontent(startIndex : String.Index) -> String.Index? {
 		guard let start = content.range(of: "timeseries\": [", options: .literal, range: startIndex..<content.endIndex, locale: nil)
 			else { return nil }
@@ -41,19 +64,18 @@ public class NGramLoader {
 			guard let dval = Double(d) else { break }
 			dvaldata.append(dval)
 		}
-		Data.append(dvaldata)
+		DataRelative.append(dvaldata)
 		return end.upperBound
 	}
 	
 	
 	private var content = ""
+		
 	public func loadData() {
-		Data = []
+		DataRelative = []
 		if let url = URL(string: url ) {
 			do {
 				content = try String(contentsOf: url)
-				//print(content)
-				
 				var index : String.Index? = content.startIndex
 				while index != nil {
 					index = parsecontent(startIndex: index!)
